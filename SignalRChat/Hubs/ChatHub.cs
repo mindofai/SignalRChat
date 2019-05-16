1ec6d9ffffae29241c048aa86b9d567d34568d80
Bryan Anthony Garcia <bryan@xam-consulting.com>
2019-05-16 21:29:39 +1000

    Add project files.

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(string user)
+        {
+            await Clients.All.SendAsync("JoinChat", user);
+        }
+
+        public async Task LeaveChat(string user)
+        {
+            await Clients.All.SendAsync("LeaveChat", user);
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+    }
+}

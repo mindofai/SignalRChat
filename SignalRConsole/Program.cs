8e8a95c538a1ddb131ad459e1d96c64e7cea878b
Bryan Anthony Garcia <bryan@xam-consulting.com>
2019-05-17 16:07:25 +1000

    Added

---
+++ SignalRConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRConsole
+{
+    class Program
+    {
+        private static HubConnection hubConnection;
+
+        static void Main(string[] args)
+        {
+            hubConnection = new HubConnectionBuilder().WithUrl("https://localhost:44398/chathub").Build();
+
+            hubConnection.StartAsync().Wait();
+
+            hubConnection.On<string, string>("ReceiveMessage", (user, receivedMessage) =>
+            {
+                Console.WriteLine($"{user} said {receivedMessage}");
+            });
+
+
+            Console.Write("Type any message: ");
+            var keepGoing = true;
+            do
+            {
+                var text = Console.ReadLine();
+                if (text == "exit")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    hubConnection.SendAsync("SendMessage", "SampleUser", text).Wait();
+                }
+            }
+            while (keepGoing);
+
+            Console.ReadKey();
+        }
+    }
+}
